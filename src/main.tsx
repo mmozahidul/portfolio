c7a3557fdc1a58caa3c0fd72bd14d153fd001aa2
mmozahidul <mmozahiduli@gmail.com>
2025-07-12 17:14:08 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,18 @@
-import buffer from 'buffer';
-window.Buffer = buffer.Buffer;
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import Portfolio from './components/Portfolio.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="*" element={<App />} />
-      </Routes>
-    </Router>
-  </StrictMode>
+// This is the correct way to add the Buffer helper
+import { Buffer } from 'buffer';
+globalThis.Buffer = Buffer;
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
